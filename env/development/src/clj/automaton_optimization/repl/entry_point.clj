df177c54465dfdb57f182dadc02ebe4eb1ee79e8
kaspazza <kaspazza@gmail.com>
2024-03-12 15:43:52 +0100

    Regenerate files

--- env/development/src/clj/automaton_optimization/repl/entry_point.clj
+++ env/development/src/clj/automaton_optimization/repl/entry_point.clj
@@ -3,21 +3,16 @@
   (:require
    [automaton-core.log :as core-log]
    [automaton-core.repl :as core-repl]
-   [cider.nrepl :as cider-nrepl]
-   [refactor-nrepl.middleware]
    [mount.core :as mount]
    [mount.tools.graph :as graph])
   (:gen-class))
 
-(defn- stub [& _args] nil)
+(defn- stub [& _args] (constantly nil))
 
 (defn -main
   "Main entry point for repl"
   [& args]
   (core-log/info "Start `automaton-simulation-de`")
   (core-log/trace "Component dependencies: " (graph/states-with-deps))
-  (core-repl/start-repl args
-                        (conj cider-nrepl/cider-middleware
-                              'refactor-nrepl.middleware/wrap-refactor)
-                        stub)
+  (core-repl/start-repl args (core-repl/default-middleware) stub)
   (mount/start))
