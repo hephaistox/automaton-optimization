8305aac6ce32a7487ac5a7a1f0ef5ae534da534e
kaspazza <kaspazza@gmail.com>
2024-08-01 12:40:14 +0200

    automatic commit

--- env/development/src/clj/automaton_optimization/repl/entry_point.clj
+++ env/development/src/clj/automaton_optimization/repl/entry_point.clj
@@ -4,7 +4,7 @@
    [automaton-core.log  :as core-log]
    [automaton-core.repl :as core-repl]
    [mount.core          :as mount]
-   [mount.tools.graph   :as graph])
+   [mount.tools.graph   :as mount-graph])
   (:gen-class))
 
 (defn- stub [& _args] (constantly nil))
@@ -13,6 +13,6 @@
   "Main entry point for repl"
   [& args]
   (core-log/info "Start `automaton-simulation-de`")
-  (core-log/trace "Component dependencies: " (graph/states-with-deps))
+  (core-log/trace "Component dependencies: " (mount-graph/states-with-deps))
   (core-repl/start-repl args (core-repl/default-middleware) stub)
   (mount/start))
